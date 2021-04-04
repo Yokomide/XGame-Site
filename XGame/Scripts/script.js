ba86adf4030e54eae42320dffa5bd5d69186d9a5
Yokomi <38473135+Yokomide@users.noreply.github.com>
2021-04-04 22:08:06 +0300

    ,

--- XGame/Scripts/script.js
+++ XGame/Scripts/script.js
@@ -3,7 +3,6 @@ var modal = document.getElementById("myModal");
 
 var btn = document.getElementById("myBtn");
 
-
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
